692aeafed72241ed4626e8dfc5b5ce246c384de0
amir_barak <binary.eyes.studio@gmail.com>
2025-01-31 21:28:19 +0200

    fixed issue with the text_type in the text-node class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print('hello, world!')
+from textnode import TextType, TextNode
+
+def main():
+    node = TextNode('Malinois Bytes', TextType.BOLD, 'https://mal.bytes.com')
+    print(node)
+
+main()
