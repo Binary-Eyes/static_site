c14e238ac1b3de06a6c8805c1e0e992311ef99ed
amir_barak <binary.eyes.studio@gmail.com>
2025-02-05 00:30:40 +0200

    adding the text-node class

--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,10 @@ class NodeType(Enum):
     CODE = "code"
     LINKS = "links"
     IMAGES = "images"
+
+
+class TextNode():
+    def __Initi__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
