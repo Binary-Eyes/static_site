2ee2b89b7c968aa15923982a12a06f706857a88e
amir_barak <binary.eyes.studio@gmail.com>
2025-02-05 23:54:02 +0200

    testing converation utility

--- src/conversions.py
+++ src/conversions.py
@@ -3,4 +3,11 @@ from leafnode import LeafNode
 
 def text_node_to_html_node(text_node: TextNode):
     type = text_node.text_type
-    if type == Text
+    if type == TextNodeType.NORMAL:
+        return LeafNode(tag=None, value=text_node.text)
+    if type == TextNodeType.BOLD:
+        return LeafNode(tag='b', value=text_node.text)
+    if type == TextNodeType.ITALIC:
+        return LeafNode(tag='i', value=text_node.text)
+    pass
+    
---
+++ src/test_conversations.py
@@ -0,0 +1,6 @@
+import unittest
+
+from conversions import text_node_to_html_node
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    pass
