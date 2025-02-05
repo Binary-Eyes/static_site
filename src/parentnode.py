735b987a430598e56c59f2da0a3b85f10542fb12
amir_barak <binary.eyes.studio@gmail.com>
2025-02-05 23:27:01 +0200

    more tests for the parent-node

--- src/parentnode.py
+++ src/parentnode.py
@@ -8,4 +8,7 @@ class ParentNode(HtmlNode):
         if self.tag is None or self.tag == '':
             raise ValueError('parent node must contain a tag')
         
+        if self.children is None or len(self.children) == 0:
+            raise ValueError('parent node must contain at least one child')
+        
         pass
--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -10,3 +10,11 @@ class TestParentNode(unittest.TestCase):
     def test_node_with_empty_tag(self):
         node = ParentNode("", [])
         self.assertRaises(ValueError, node.to_html)
+
+    def test_node_with_no_children(self):
+        node = ParentNode("a", None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_node_with_zero_children(self):
+        node = ParentNode("h1", [])
+        self.assertRaises(ValueError, node.to_html)
