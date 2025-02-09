6cdd532fb4cdf4438db7b347439b9045457cdefa
amir_barak <binary.eyes.studio@gmail.com>
2025-02-09 20:53:35 +0200

    tested very simple case for splitting a single text node with no delimiters:

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -10,14 +10,14 @@ class TestSplittingSimpleTextNode(unittest.TestCase):
         self.assertEqual(len(split), 1)
         pass
     
-    # def test_should_return_node_with_expected_text(self):
-    #     node = TextNode("simple text", TextType.TEXT)
-    #     split = split_nodes_delimiter(node, '*', TextType.ITALIC)
-    #     self.assertEqual(split[0].text, "simple text")
-    #     pass
+    def test_should_return_node_with_expected_text(self):
+        node = TextNode("simple text", TextType.TEXT)
+        split = split_nodes_delimiter([node], '*', TextType.ITALIC)
+        self.assertEqual(split[0].text, "simple text")
+        pass
 
-    # def test_should_return_node_with_text_type(self):
-    #     node = TextNode("simple text", TextType.TEXT)
-    #     split = split_nodes_delimiter(node, '*', TextType.ITALIC)
-    #     self.assertEqual(split[0].node_type, TextType.TEXT)
-    #     pass
+    def test_should_return_node_with_text_type(self):
+        node = TextNode("simple text", TextType.TEXT)
+        split = split_nodes_delimiter([node], '*', TextType.ITALIC)
+        self.assertEqual(split[0].node_type, TextType.TEXT)
+        pass
