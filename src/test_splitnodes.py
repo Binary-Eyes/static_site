edb216b6b6f2368cf6ff32a13205697ba6e39754
amir_barak <binary.eyes.studio@gmail.com>
2025-02-09 20:57:18 +0200

    testing code block

--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -3,6 +3,13 @@ import unittest
 from textnode import TextNode, TextType
 from conversions import split_nodes_delimiter
 
+class TestSplittingTextNodeWithOneCodeBlock(unittest.TestCase):
+    def test_should_return_three_nodes(self):
+        node = TextNode("the code snippet 'float a = 1.0' sets a floating-point value")
+        split = split_nodes_delimiter([node], "'", TextType.CODE)
+        self.assertEqual(len(split), 3)
+        pass
+
 class TestSplittingSimpleTextNode(unittest.TestCase):
     def test_should_return_expected_lenth(self):
         node = TextNode("simple text", TextType.TEXT)
