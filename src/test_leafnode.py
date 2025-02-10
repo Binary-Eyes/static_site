80b2622ef420dd345b8fe33456788a89ebe6f583
amir_barak <binary.eyes.studio@gmail.com>
2025-02-10 09:06:26 +0200

    reverted weird changes to the leaf-node tests

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -3,7 +3,17 @@ import unittest
 from leafnode import LeafNode
 
 class TestLeafNode(unittest.TestCase):
-    def test_node_with_no_value(self):
-        node = LeafNode()
-        self.assertRaises()
-        
+    def test_leaf_node_with_tag(self):
+        node = LeafNode('p', "this is a test paragraph!")
+        html = node.to_html()
+        self.assertEqual(html, '<p>this is a test paragraph!</p>')
+
+    def test_leaf_node_without_tag(self):
+        node = LeafNode(None, "raw text dog")
+        html = node.to_html()
+        self.assertEqual(html, "raw text dog")
+
+    def test_leaf_node_will_all(self):
+        node = LeafNode('a', 'click me!', {"href": "www.clickme.com"})
+        html = node.to_html()
+        self.assertEqual(html, '<a href="www.clickme.com">click me!</a>')
