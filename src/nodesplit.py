20c8b3e83bfeba8fd9e1054f04de9219091cc1f8
amir_barak <binary.eyes.studio@gmail.com>
2025-02-10 09:34:41 +0200

    maybe working?

--- src/nodesplit.py
+++ src/nodesplit.py
@@ -14,13 +14,16 @@ def split_nodes_delimiter(source_nodes, delimiter, text_type):
             continue
 
         split_text = source_node.text.split(delimiter)
+        if len(split_text)%2 == 0:
+            raise ValueError(f"requested text for parsing has invalid delimiter count\nTEXT={source_node.text}\nDELIMITER={delimiter}")
+
         for i in range(0, len(split_text)):
             node = None
             if i%2 == 0:
                 node = TextNode(split_text[i], TextType.TEXT)
             else:
                 node = TextNode(split_text[i], text_type)
-
+            
             target_nodes.append(node)
 
     return target_nodes
