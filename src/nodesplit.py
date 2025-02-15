12801c8a51eca99b25d4b847d4016d734e89d745
amir_barak <binary.eyes.studio@gmail.com>
2025-02-15 20:41:47 +0200

    tested node splitters

--- src/nodesplit.py
+++ src/nodesplit.py
@@ -1,15 +1,53 @@
 from textnode import TextNode, TextType
 from extractions import extract_markdown_links, extract_markdown_images
 
+def split_nodes_image(source_nodes):
+    split_nodes = []    
+    for source_node in source_nodes:
+        images = extract_markdown_images(source_node.text)
+        total_images = len(images)
+        if total_images == 0:
+            if len(source_node.text) > 0:
+                split_nodes.append(source_node)
+            continue
+
+        image = images[0]
+        image_text = f'![{image[0]}]({image[1]})'
+        split_text = source_node.text.split(image_text, 1)
+        if len(split_text) != 2:
+            raise Exception("splitting text failed")
+
+        if len(split_text[0]) > 0:            
+            split_nodes.append(TextNode(split_text[0], TextType.TEXT))
+
+        split_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+        last = TextNode(split_text[1], TextType.TEXT)
+        split_nodes.extend(split_nodes_link([last]))
+
+    return split_nodes
+
 def split_nodes_link(source_nodes):
     split_nodes = []    
     for source_node in source_nodes:
         links = extract_markdown_links(source_node.text)
         total_links = len(links)
-        if total_links == 0:
-            split_nodes.append(source_node)
-        else:
+        if total_links == 0:                
+            if len(source_node.text) > 0:
+                split_nodes.append(source_node)
             continue
+
+        link = links[0]
+        link_text = f'[{link[0]}]({link[1]})'
+        split_text = source_node.text.split(link_text, 1)
+        if len(split_text) != 2:
+            raise Exception("splitting text failed")
+
+        if len(split_text[0]) > 0:            
+            split_nodes.append(TextNode(split_text[0], TextType.TEXT))
+
+        split_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+        last = TextNode(split_text[1], TextType.TEXT)
+        split_nodes.extend(split_nodes_link([last]))
 
     return split_nodes
 
--- src/test_splitlink.py
+++ src/test_splitlink.py
@@ -1,7 +1,21 @@
 import unittest
 
 from textnode import TextNode, TextType
-from nodesplit import split_nodes_link
+from nodesplit import split_nodes_link, split_nodes_image
+
+class TestSplittingNodeWithImage(unittest.TestCase):
+    def test_should_return_expected(self):
+        node = TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)", TextType.TEXT)
+        split = split_nodes_image([node])
+        self.assertEqual(len(split), 2)
+
+
+class TestSplittingNodeWithMultipleLinks(unittest.TestCase):
+    def test_should_return_expected(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        self.assertEqual(len(nodes), 4)
+
 
 class TestSplittingNodeWithOneLink(unittest.TestCase):
     def test_should_return_expected_node_count(self):
