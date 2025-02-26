9df183b88461ffaa43a4e3907e9baf2dd9f71f5e
amir_barak <binary.eyes.studio@gmail.com>
2025-02-26 18:55:44 +0200

    even more tests for the parent node class

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -33,3 +33,15 @@ class TestParentWithMultipleChildren(unittest.TestCase):
         html = node.to_html()
         self.assertEqual(html, '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>')
 
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>")
