0541f95abb870a8c33b8317fb04492eab1fc7828
amir_barak <binary.eyes.studio@gmail.com>
2025-02-26 18:51:40 +0200

    testing parent-node to html

--- src/parentnode.py
+++ src/parentnode.py
@@ -11,4 +11,10 @@ class ParentNode(HtmlNode):
         if self.children is None or len(self.children) == 0:
             raise ValueError('parent node is missing children')
         
-        
+        props = self.props_to_html()
+        html = f'<{self.tag}{props}>'
+        for child in self.children:
+            html += child.to_html()
+
+        html += f'</{self.tag}>'
+        return html
--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from parentnode import *
+from leafnode import *
 
 class TestParentNodeWithoutTag(unittest.TestCase):
     def test_should_fail(self):
@@ -11,3 +12,12 @@ class TestParentNodeWithNoChildren(unittest.TestCase):
     def test_should_fail(self):
         node = ParentNode('a', None, None)
         self.assertRaises(ValueError, node.to_html)
+
+class TestParentWithOneChild(unittest.TestCase):
+    def test_should_return_expected_for_child_no_props(self):
+        child_node = LeafNode('b', 'do not')
+        node = ParentNode('a', [child_node])
+        html = node.to_html()
+        self.assertEqual(html, '<a><b>do not</b></a>')
+
+
